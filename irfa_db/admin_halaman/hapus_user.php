7aae26cde9b917ca32a21d8ce3e35d247c1e8dba
codemetik <45462881+codemetik@users.noreply.github.com>
2019-07-04 01:18:59 +0700

    Delete hapus_user.php

--- irfa_db/admin_halaman/hapus_user.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include "../koneksi.php";
-
-$id = $_GET['id'];
-$sql = mysqli_query($koneksi, "DELETE FROM at_uset WHERE id_user ='".$id."'");
-
-header("location:../admin_dasboard.php?page=data_user");
-?>
